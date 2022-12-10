3cee1e9486add1d9a6f3a728cb2074219d26bc19
loic10 <loicnguegang@gmail.com>
2022-12-10 02:50:29 +0100

    update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+  @Get("users")
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
